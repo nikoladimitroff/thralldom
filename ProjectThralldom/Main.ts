9266a692c5faf85c4a003ffcbad1f7e3a1804fac
NikolaDimitroff <dimitroff.nikola@gmail.com>
2014-01-08 00:24:01 +0200

    Project setup

---
+++ ProjectThralldom/Main.ts
@@ -0,0 +1,3 @@
+/// <reference path="application.ts" />
+var app: Thralldom.Application = new Thralldom.Application();
+app.run();
---
+++ ProjectThralldom/Tests/Tests.ts
@@ -0,0 +1,5 @@
+module Thralldom.Tests {
+    class Tests {
+
+    }
+}
