c116a83b985792b49babb5668cc6938cf4990498
Nikola DimitrofF <dimitroff.nikola@gmail.com>
2014-04-08 10:33:21 +0300

    Pause screen added and working flawlessly

--- ProjectThralldom/Code/Managers/UIManager.ts
+++ ProjectThralldom/Code/Managers/UIManager.ts
@@ -3,11 +3,12 @@ module Thralldom {
 
         public text: HTMLElement;
         public subtitles: HTMLElement;
-
+        public pausedScreen: HTMLElement;
         
         constructor() {
             this.text = <HTMLElement> document.querySelector("nav pre");
             this.subtitles = <HTMLElement> document.querySelector("#subtitles span");
+            this.pausedScreen = <HTMLElement> document.querySelector("#paused-screen");
         }
     }
 }
